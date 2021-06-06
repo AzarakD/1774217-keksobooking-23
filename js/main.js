7480b08dca244ac1a8dd841f3242fc6b1802eb61
Azarak <abbatd@gmail.com>
2021-06-06 17:26:44 +1000

    Исправил функции рандомных чисел

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-function getRandomInteger(min, max) {
+const getRandomInteger = (min, max) => {
   if (min >= 0 && max >= 0 && min < max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  return false;
-}
+  return 'Задан неверный диапазон. Минимум должен быть меньше максимума, оба числа больше ноля';
+};
 
-function getRandomFloat(min, max, signs) {
+const getRandomFloat = (min, max, signs) => {
   if (min >= 0 && max >= 0 && min < max) {
     const divider = Math.pow(10, signs);
     return Math.floor((Math.random() * (max - min + (1 / divider)) + min) * divider) / divider;
   }
 
-  return false;
-}
+  return 'Задан неверный диапазон. Минимум должен быть меньше максимума, оба числа больше ноля';
+};
 
 getRandomInteger(0, 10);
 getRandomFloat(1, 10, 2);
