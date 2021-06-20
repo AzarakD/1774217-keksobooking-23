f03877ee73f2f851b91fd8482e4590c82a0488f8
Azarak <abbatd@gmail.com>
2021-06-21 00:43:27 +1000

    Добавил модуль состояния страницы

--- js/main.js
+++ js/main.js
@@ -2,17 +2,21 @@ import { generateOffer } from './generate-offer.js';
 import { generateCard } from './popup-card.js';
 import { activatePage } from './page-state.js';
 
-const OFFERS = 10;
-const generatedOffers = new Array(OFFERS).fill('').map(() => generateOffer());
-const generatedCards = new Array(OFFERS).fill('');
-const cardsFragments = document.createDocumentFragment();
+const OFFERS = 1;
 
-for (let offer = 0; offer < OFFERS; offer++) {
-  generatedCards[offer] = generateCard(generatedOffers[offer]);
-  cardsFragments.appendChild(generatedCards[offer]);
-}
+const placeOfferOnMap = () => {
+  const generatedOffers = new Array(OFFERS).fill('').map(() => generateOffer());
+  const generatedCards = new Array(OFFERS).fill('');
+  const cardsFragments = document.createDocumentFragment();
 
-const mapCanvas = document.querySelector('#map-canvas');
-mapCanvas.appendChild(cardsFragments.firstChild);
+  for (let offer = 0; offer < OFFERS; offer++) {
+    generatedCards[offer] = generateCard(generatedOffers[offer]);
+    cardsFragments.appendChild(generatedCards[offer]);
+  }
 
+  const mapCanvas = document.querySelector('#map-canvas');
+  mapCanvas.appendChild(cardsFragments);
+};
+
+placeOfferOnMap();
 activatePage();
